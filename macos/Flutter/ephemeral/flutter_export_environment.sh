7bcb477e1f33b50d18da33174573f62d6b786b2a
DTI <projetosdti04@gmail.com>
2026-03-20 18:15:59 -0400

    feat: implementa validacao de telefone na tela de cadastro

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\55959\Documents\app_pole_fenix"
+export "FLUTTER_ROOT=C:\flutter_windows_3.27.1-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Beatriz\Documents\app_pole_fenix"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
